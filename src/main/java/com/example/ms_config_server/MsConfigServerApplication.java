e06fb48d676e3b3822843a339863c9977aca99c8
Alisson <arteaga10alisson@gmail.com>
2022-07-31 20:22:03 -0500

    add bootstrap and eureka client

--- src/main/java/com/example/ms_config_server/MsConfigServerApplication.java
+++ src/main/java/com/example/ms_config_server/MsConfigServerApplication.java
@@ -3,11 +3,12 @@ package com.example.ms_config_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class MsConfigServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MsConfigServerApplication.class, args);
     }
